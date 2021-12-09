adce01cc568e2de376a1f94eab4b8a0986d9551e
RafaelAfrikyan <rafaelafrikyan95@gmail.com>
2021-12-09 21:27:40 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 1. Concate Arrays
+
+Concatenate the two arrays.
+
+[concateArr.js](concateArr.js)
---
+++ concateArr.js
@@ -0,0 +1,6 @@
+// Concatenate the two arrays.
+
+let arr1 = [1, 2, 3, 4];
+let arr2 = [5, 6, 7, 8, 9];
+let result = arr1.concat(arr2);
+console.log(result);
